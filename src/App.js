c5013ae4faef68e1698980ea3c505c7fcee1d801
Adhyas <satheeshkumar@1000lookz.com>
2020-01-04 13:07:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import styles from "./App.module.css";
+const filterData = [
+  "ITEM1",
+  "ITEM2",
+  "ITEM3",
+  "ITEM4",
+  "ITEM5",
+  "ITEM6",
+  "ITEM7",
+  "ITEM8",
+  "ITEM9",
+  "ITEM10",
+  "ITEM11"
+];
+class App extends Component {
+  state = {
+    selectedFilter: "",
+    toolTipCss: {},
+    prevScroll: 0,
+    openModal: false
+  };
+  componentDidMount() {
+    window.addEventListener(
+      "scroll",
+      () => {
+        let { toolTipCss, prevScroll } = this.state;
+        let { scrollLeft } = document.querySelector("#filter");
+        let left = toolTipCss.orgLeft
+          ? toolTipCss.orgLeft + prevScroll - scrollLeft
+          : 0;
+        this.setState({
+          toolTipCss: { ...toolTipCss, left: `${left}px`, orgLeft: left },
+          prevScroll: scrollLeft
+        });
+      },
+      true
+    );
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  filterClickHandler = (e, name) => {
+    let { openModal, selectedFilter } = this.state;
+    const pos = ReactDOM.findDOMNode(e.target).getBoundingClientRect();
+    const left = pos.left;
+    const toolTipCss = {
+      display: "block",
+      left: `${left}px`,
+      orgLeft: left
+    };
+    let updateModal = !openModal;
+    if (selectedFilter !== name) {
+      updateModal = true;
+    }
+    this.setState({ toolTipCss, selectedFilter: name, openModal: updateModal });
+  };
+
+  render() {
+    const { selectedFilter, toolTipCss, openModal } = this.state;
+    const filterList = filterData.map((filter, i) => {
+      return (
+        <div
+          key={i}
+          className={styles.filterItem}
+          onClick={e => {
+            this.filterClickHandler(e, filter);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {filter}
+        </div>
+      );
+    });
+    return (
+      <div className={styles.container}>
+        <div className={styles.filterContainer} id="filter">
+          {filterList}
+        </div>
+        <div
+          className={styles.filterCategory}
+          style={{ display: openModal ? "flex" : "none" }}
+        >
+          <div className={styles.tooltip} style={{ ...toolTipCss }}></div>
+          {selectedFilter}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
